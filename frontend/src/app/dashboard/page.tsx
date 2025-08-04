93777564c03367193711dc974de07a878f43a422
radityadias <radityadias24@gmail.com>
2025-08-04 18:32:54 +0700

    update navbar

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,13 +1,10 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+  UserIcon
+} from "lucide-react";
 import {
   SidebarInset,
   SidebarProvider,
@@ -20,22 +17,17 @@ export default function Page() {
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex items-center gap-2 px-3">
+          <div className="flex items-center gap-2 px-3 w-full">
             <SidebarTrigger />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <div className="grid w-full">
+              <div className="flex justify-between gap-2">
+                <Input placeholder="Search..." className="max-w-96 transition-all duration-300 sm:focus:max-w-[30vw] " />
+                <Button size="icon" variant="icon">
+                  <UserIcon/>
+                </Button>
+              </div>
+            </div>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        icon: "border rounded-full bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
