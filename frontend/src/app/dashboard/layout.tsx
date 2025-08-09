d6a80ed0f5b9593567af8d6a03390af18418ddbd
radityadias <radityadias24@gmail.com>
2025-08-09 20:53:02 +0700

    add footer

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import AppFooter  from "@/components/app-footer"
 import { Separator } from "@/components/ui/separator"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -14,28 +15,33 @@ import {
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarRail />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b ">
-                    <div className="flex items-center gap-2 w-full max-w-[1280px] mx-auto px-4 sm:px-4 lg:px-0">
-                        <SidebarTrigger />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
-                        <div className="grid w-full">
-                            <div className="flex justify-between gap-2">
-                                <Input placeholder="Search..." className="max-w-96 transition-all duration-300 sm:focus:max-w-[30vw] " />
-                                <Button size="icon" variant="icon">
-                                    <UserIcon/>
-                                </Button>
+        <>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarRail />
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 border-b ">
+                        <div className="flex items-center gap-2 w-full max-w-[1280px] mx-auto px-4 sm:px-4 lg:px-0">
+                            <SidebarTrigger />
+                            <Separator orientation="vertical" className="mr-2 h-4" />
+                            <div className="grid w-full">
+                                <div className="flex justify-between gap-2">
+                                    <Input placeholder="Search..." className="max-w-96 transition-all duration-300 sm:focus:max-w-[30vw] " />
+                                    <Button size="icon" variant="icon">
+                                        <UserIcon/>
+                                    </Button>
+                                </div>
                             </div>
                         </div>
+                    </header>
+                    <div className="flex flex-1 flex-col gap-4 p-4">
+                        {children}
                     </div>
-                </header>
-                <div className="flex flex-1 flex-col gap-4 p-4">
-                    {children}
-                </div>
-            </SidebarInset>
-        </SidebarProvider>
+                    <AppFooter />
+                </SidebarInset>
+            </SidebarProvider>
+
+        </>
+
     )
 }
